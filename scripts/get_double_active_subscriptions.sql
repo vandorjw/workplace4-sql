a82e99c50c1453973106b5aca1c29100c2c8b3d7
Joost Vandorp <joostvandorp@gmail.com>
2019-11-12 10:38:34 -0500

    Create get_double_active_subscriptions.sql

---
+++ scripts/get_double_active_subscriptions.sql
@@ -0,0 +1,35 @@
+-- DOUBLE ACTIVE SUBSCRIPTIONS
+ select
+	s.subscription_id,
+	o.order_id,
+	s.next_billing_date as sub_billing_date,
+	o.billing_date as order_billing_date,
+	o.status as order_status,
+	s.status as subscription_status,
+	o.first_name,
+	o.last_name
+from
+	subscriptions s
+right join orders o on
+	s.subscription_id = o.subscription_id
+where
+	s.subscription_id in (
+	select
+		double_active.subscription_id
+	from
+		(
+		select
+			s.subscription_id,
+			count(*) as amount
+		from
+			orders o
+		left join subscriptions s on
+			o.subscription_id = s.subscription_id
+		where
+			o.status = 'Active'
+			and s.subscription_id is not NULL
+		group by
+			s.subscription_id
+		having
+			amount > 1) as double_active)
+order by s.subscription_id;
