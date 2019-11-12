c30e80602a1da5f9a15e227a4968b19e6f376928
Joost Vandorp <joostvandorp@gmail.com>
2019-11-12 10:42:13 -0500

    Create fix_subscription_next_billing_date.sql

---
+++ fix_subscription_next_billing_date.sql
@@ -0,0 +1,31 @@
+subscriptions s
+SET
+    s.next_billing_date =
+    CASE
+        -- EST --
+        WHEN s.zone_id IN ('1','4') 
+            THEN IF(TIME(s.next_billing_date) < '18:00:00',
+                TIMESTAMP(DATE(s.next_billing_date), TIME('10:00')),
+                TIMESTAMP(DATE_ADD(DATE(s.next_billing_date), INTERVAL 1 DAY), TIME('10:00'))
+                )
+        WHEN s.zone_id IN ('5') 
+            THEN IF(TIME(s.next_billing_date) < '19:00:00',
+                TIMESTAMP(DATE(s.next_billing_date), TIME('10:00')),
+                TIMESTAMP(DATE_ADD(DATE(s.next_billing_date), INTERVAL 1 DAY), TIME('10:00'))
+                )
+        -- MST --
+        WHEN s.zone_id IN ('2') 
+            THEN IF(TIME(s.next_billing_date) < '20:00:00',
+                TIMESTAMP(DATE(s.next_billing_date), TIME('10:00')),
+                TIMESTAMP(DATE_ADD(DATE(s.next_billing_date), INTERVAL 1 DAY), TIME('10:00'))
+                )
+        -- PST --
+        WHEN s.zone_id IN ('3') 
+            THEN IF(TIME(s.next_billing_date) < '21:00:00',
+                TIMESTAMP(DATE(s.next_billing_date), TIME('10:00')),
+                TIMESTAMP(DATE_ADD(DATE(s.next_billing_date), INTERVAL 1 DAY), TIME('10:00'))
+                )
+    END
+WHERE s.status IN ('Active', 'Skipped')
+and s.next_billing_date is not NULL
+and s.next_billing_date != '0000-00-00 00:00:00';
