e8fb71be7c902fc66a34c16499d46b40fa6f58c2
Joost Vandorp <joostvandorp@gmail.com>
2019-11-12 14:26:27 -0500

    Create get_subscriptions_billing_date_less_than_MAX_order.sql

---
+++ scripts/get_subscriptions_billing_date_less_than_MAX_order.sql
@@ -0,0 +1,21 @@
+select
+	o.subscription_id, 
+	o.order_id,
+	o.status as o_status,
+	s.status as s_status,
+	o.billing_date,
+	s.next_billing_date,
+	CASE
+		WHEN s.next_billing_date > o.billing_date THEN "t"
+		ELSE "f"
+	END as sub_gt_order
+from orders o left join subscriptions s 
+on o.subscription_id = s.subscription_id
+where
+o.order_id in (select order_id from (select MAX(o.billing_date), o.order_id, o.status from orders o 
+where o.subscription_id is not NULL
+and o.status != 'Active'
+group by o.subscription_id) as max_not_active)
+and o.billing_date != s.next_billing_date
+and s.status='Active'
+HAVING sub_gt_order="f";
