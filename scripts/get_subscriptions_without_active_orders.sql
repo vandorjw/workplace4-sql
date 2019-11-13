ef7f49bb57b9a0c47f5f971fb6baf3aa88d9fd51
Joost Vandorp <joostvandorp@gmail.com>
2019-11-12 22:15:33 -0500

    Create get_subscriptions_without_active_orders.sql

---
+++ scripts/get_subscriptions_without_active_orders.sql
@@ -0,0 +1,39 @@
+-- get the subscription ID for all subscriptions that need to re-create an active order object
+SELECT
+	s.subscription_id
+FROM
+	subscriptions s
+WHERE
+	s.status = 'Active'
+AND NOT 
+	s.subscription_id IN (
+		SELECT
+			o.subscription_id
+		FROM
+			orders o
+		WHERE o.status IN ('Active', 'Failed Payment 1', 'Failed Payment 2', 'Declined')
+		AND o.subscription_id IS NOT NULL
+	)
+AND NOT
+	s.subscription_id IN (
+		SELECT
+			o.subscription_id
+		FROM
+			orders o
+		WHERE o.status = 'Processed'
+		AND o.order_id IN (
+			SELECT
+				i.order_id
+			FROM
+				invoices i
+			WHERE i.order_id IS NOT NULL
+			AND i.status IN ('Unfulfilled', 'InProgress')
+		)
+	)
+AND s.subscription_id IN (
+	SELECT
+		o.subscription_id
+	FROM
+		orders o
+	WHERE
+		o.subscription_id IS NOT NULL);
