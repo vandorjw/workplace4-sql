a9936531f70446173bba1c6d056c58ae98f22e3e
Joost Vandorp <joostvandorp@gmail.com>
2019-11-13 15:30:51 -0500

    Update get_subscriptions_without_active_orders.sql

--- scripts/get_subscriptions_without_active_orders.sql
+++ scripts/get_subscriptions_without_active_orders.sql
@@ -1,40 +1,57 @@
 -- get the subscription ID for all subscriptions that need to re-create an active order object
 SELECT
-	s.subscription_id
+	`subscriptions`.`subscription_id`
 FROM
-	subscriptions s
+	`subscriptions`
 WHERE
-	s.status = 'Active'
-AND NOT 
-	s.subscription_id IN (
-		SELECT
-			o.subscription_id
-		FROM
-			orders o
-		WHERE o.status IN ('Active', 'Failed Payment 1', 'Failed Payment 2', 'Declined')
-		AND o.subscription_id IS NOT NULL
-	)
-AND NOT
-	s.subscription_id IN (
-		SELECT
-			o.subscription_id
-		FROM
-			orders o
-		WHERE o.status = 'Processed'
-		AND o.subscription_id IS NOT NULL
-		AND o.order_id IN (
-			SELECT
-				i.order_id
-			FROM
-				invoices i
-			WHERE i.order_id IS NOT NULL
-			AND i.status IN ('Unfulfilled', 'InProgress')
-		)
-	)
-AND s.subscription_id IN (
+	(`subscriptions`.`status` = 'Active'
+	AND NOT (`subscriptions`.`subscription_id` IN (
 	SELECT
-		o.subscription_id
+		U0.`subscription_id`
 	FROM
-		orders o
+		`orders` U0
 	WHERE
-		o.subscription_id IS NOT NULL);
+		(U0.`status` IN ('Active',
+		'Failed Payment 1',
+		'Failed Payment 2',
+		'Declined')
+		AND U0.`subscription_id` IS NOT NULL)))
+	AND NOT (`subscriptions`.`subscription_id` IN (
+	SELECT
+		V0.`subscription_id`
+	FROM
+		`orders` V0
+	WHERE
+		(V0.`status` = 'Processed'
+		AND V0.`subscription_id` IS NOT NULL
+		AND V0.`order_id` IN (
+		SELECT
+			U0.`order_id`
+		FROM
+			`invoices` U0
+		WHERE
+			(U0.`order_id` IS NOT NULL
+			AND U0.`status` IN ('Unfulfilled',
+			'InProgress'))))))
+	AND `subscriptions`.`subscription_id` IN (
+	SELECT
+		U0.`subscription_id`
+	FROM
+		`orders` U0
+	WHERE
+		U0.`subscription_id` IS NOT NULL)
+	AND NOT (`subscriptions`.`subscription_id` IN (
+	SELECT
+		V0.`subscription_id`
+	FROM
+		`orders` V0
+	WHERE
+		(V0.`status` = 'Processed'
+		AND V0.`subscription_id` IS NOT NULL
+		AND NOT (V0.`order_id` IN (
+		SELECT
+			U0.`order_id`
+		FROM
+			`invoices` U0
+		WHERE
+			U0.`order_id` IS NOT NULL))))));
