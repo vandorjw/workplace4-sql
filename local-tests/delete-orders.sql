da7f94c4c6632a959c0b6371e1ee76d7135150f8
Joost Vandorp <joostvandorp@gmail.com>
2019-11-29 08:46:32 -0500

    Update delete-orders.sql

--- local-tests/delete-orders.sql
+++ local-tests/delete-orders.sql
@@ -12,11 +12,34 @@ delete o.* from orders o where
 and o.status='Active';
 
 
--- everything
-
+-- cancelled subs
 delete op.* from order_products op
 where op.order_id in (
-	select distinct(o.order_id) from orders o
-);
+select distinct(o.order_id) from subscriptions s left join orders o on 
+s.subscription_id=o.subscription_id
+where s.status='Cancelled');
 
-delete o.* from orders o;
+
+delete o.* from subscriptions s left join orders o on 
+s.subscription_id=o.subscription_id
+where s.status='Cancelled';
+
+
+delete sa.* from subscriptions s left join subscription_addons sa on 
+s.subscription_id=sa.subscription_id
+where s.status='Cancelled';
+
+
+delete sa.* from subscriptions s left join surveys on 
+s.subscription_id=surveys.subscription_id
+left join survey_answers sa on
+surveys.survey_id=sa.survey_id
+where s.status='Cancelled';
+
+
+delete surveys.* from subscriptions s left join surveys on 
+s.subscription_id=surveys.subscription_id
+where s.status='Cancelled';
+
+delete s.* from subscriptions s where s.status='Cancelled';
+
