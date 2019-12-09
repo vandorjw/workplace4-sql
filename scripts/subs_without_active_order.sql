6f35ad7903e9966f65867a63bad7e5a89a045107
Joost Vandorp <joostvandorp@gmail.com>
2019-12-09 11:11:29 -0500

    Update subs_without_active_order.sql

--- scripts/subs_without_active_order.sql
+++ scripts/subs_without_active_order.sql
@@ -1,3 +1,5 @@
+-- FIND subs without active orders that are not picked up by generator
+
 select
 	s.subscription_id
 from
@@ -18,5 +20,65 @@ where
 		orders o
 	where o.status = 'Active'
 	and o.subscription_id is not null)
+	
+	and s.subscription_id not in (
+	
+	SELECT
+	`subscriptions`.`subscription_id`
+FROM
+	`subscriptions`
+WHERE
+	(`subscriptions`.`status` = 'Active'
+	AND NOT (`subscriptions`.`subscription_id` IN (
+	SELECT
+		U0.`subscription_id`
+	FROM
+		`orders` U0
+	WHERE
+		(U0.`status` IN ('Active',
+		'Failed Payment 1',
+		'Failed Payment 2',
+		'Declined')
+		AND U0.`subscription_id` IS NOT NULL)))
+	AND NOT (`subscriptions`.`subscription_id` IN (
+	SELECT
+		V0.`subscription_id`
+	FROM
+		`orders` V0
+	WHERE
+		(V0.`status` = 'Processed'
+		AND V0.`subscription_id` IS NOT NULL
+		AND V0.`order_id` IN (
+		SELECT
+			U0.`order_id`
+		FROM
+			`invoices` U0
+		WHERE
+			(U0.`order_id` IS NOT NULL
+			AND U0.`status` IN ('Unfulfilled',
+			'InProgress'))))))
+	AND `subscriptions`.`subscription_id` IN (
+	SELECT
+		U0.`subscription_id`
+	FROM
+		`orders` U0
+	WHERE
+		U0.`subscription_id` IS NOT NULL)
+	AND NOT (`subscriptions`.`subscription_id` IN (
+	SELECT
+		V0.`subscription_id`
+	FROM
+		`orders` V0
+	WHERE
+		(V0.`status` = 'Processed'
+		AND V0.`subscription_id` IS NOT NULL
+		AND NOT (V0.`order_id` IN (
+		SELECT
+			U0.`order_id`
+		FROM
+			`invoices` U0
+		WHERE
+			U0.`order_id` IS NOT NULL))))))
+		)	
 group by
 	s.subscription_id;
