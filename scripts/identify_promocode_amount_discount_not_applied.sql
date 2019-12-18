c6f57e67db4ffe221e01429a3a3e79d20bd8a171
Joost Vandorp <joostvandorp@gmail.com>
2019-12-18 13:20:43 -0500

    Update identify_promocode_amount_discount_not_applied.sql

--- scripts/identify_promocode_amount_discount_not_applied.sql
+++ scripts/identify_promocode_amount_discount_not_applied.sql
@@ -1,17 +1,31 @@
-select
+select o.order_id, o.subtotal, broken_subtotal.correct_subtotal, o.promo_code, broken_subtotal.percentage, broken_subtotal.amount from orders o join (select
 	o.order_id,
+	o.status,
 	o.first_name,
 	o.last_name,
 	sp.price,
 	pc.promo_code,
+	pc.duration,
 	pcp.amount,
+	pcp.percentage,
 	o.subtotal,
+	o.create_date,
 	o.status_date,
+	o.billing_date,
 	CASE
-		WHEN sp.price-round(pcp.amount) < 0 then 0
-		ELSE sp.price-round(pcp.amount)
-	END as correct_subtotal,
-	o.subtotal-(sp.price-round(pcp.amount)) as delta
+		WHEN (pcp.amount is not null and pcp.amount > 0)
+		THEN
+			CASE
+				WHEN sp.price-round(pcp.amount) < 0 THEN 0
+				ELSE sp.price-round(pcp.amount)
+			END
+		WHEN (pcp.percentage is not null and pcp.percentage > 0)
+		THEN
+			CASE
+				WHEN sp.price - sp.price*(pcp.percentage/100) < 0 THEN 0
+				ELSE sp.price - sp.price*(pcp.percentage/100)
+			END
+	END as correct_subtotal
 from
 	orders o
 left join promo_codes pc on
@@ -24,15 +38,11 @@ left join subscription_plans sp on
 	s.plan_id = sp.plan_id
 where
 	pc.tier is not null
-	and (pc.duration = '-1'
-	or pc.duration = 'forever'
-	or pc.duration is null)
-	and pcp.amount is not NULL
+	and (pcp.amount is not NULL or pcp.percentage is not NULL)
 	and pcp.plan_id = s.plan_id
 	and pcp.amount > 0
 	and o.status in ('Active')
 	and pc.status = 'active'
 having
-	round(o.subtotal) <> correct_subtotal
-order by
-	o.status_date desc;
+	round(o.subtotal) <> correct_subtotal) as broken_subtotal on 
+o.order_id=broken_subtotal.order_id;
